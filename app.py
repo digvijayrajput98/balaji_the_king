d1f3a8f784373425c72759ec55069eb5e5315a8e
Digvijay Rajput <106327554+digvijayrajput98@users.noreply.github.com>
2022-06-30 10:21:19 +0000

    added link

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask,render_template
-app = Flask(__name__)
 
+from flask import Flask,render_template,jsonify
+app = Flask(__name__)
+RESULT = [{'match':'namibia vs usa',
+        'Balaji Team':'usa',
+        'Script':'lay namibia at every odds, if namibia lambi 280 bet not with 20% limit'}]
 @app.route('/')
+
 def hello_world():
-   return render_template('home.html')
+   return render_template('home.html',result=RESULT)
+@app.route("/api/result")
+def list_result():
+  return jsonify(RESULT)
+
 
 if __name__ == '__main__':
    app.run(host = '0.0.0.0',debug = True)
